f28ae37dde0cea356e0cfd6d641d583669b8683c
user <user@test.ru>
2012-11-26 18:39:21 +0400

    Save

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Work::Application.routes.draw do
+  get "main/index"
+
+  get "main/about"
+
+  get "main/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
